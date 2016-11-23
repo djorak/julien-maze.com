879f50834c204f019c7da84c9b7832b13051e30b
Julien Mazé <julien.maze@bbc.co.uk>
2016-11-23 11:59:03 +0000

    chore: add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+echo "-- Deploying www.julien-maze.com to Github pages --"
+
+if [ "$TRAVIS" = "true" ]
+then
+    echo "-> Setting up git user"
+    git config --global user.email "travis-ci@localhost"
+    git config --global user.name "djorak"
+fi
+
+echo "-> Pushing /dist to Github pages"
+./node_modules/.bin/gh-pages --repo https://$GH_TOKEN@github.com/djorak/julien-maze.com.git
