23f6aee269ea1e7e7070d4eded473d18b03c912e
Julien Mazé <julien.maze@bbc.co.uk>
2021-10-05 22:34:02 +0100

    add webpack dev server and react-refresh HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const SRC_FOLDER = path.resolve(__dirname, 'src');
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = {
   entry: `${SRC_FOLDER}/index.jsx`,
-  mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
+  mode: isDevelopment ? 'development' : 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Julien Mazé - Senior Software Engineer',
-      template: `${SRC_FOLDER}/index.html`,
-    }),
-  ],
+  devServer: {
+    hot: true,
+    static: './dist',
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         include: SRC_FOLDER,
-        use: { loader: 'babel-loader' },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
+          },
+        },
       },
     ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Julien Mazé - Senior Software Engineer',
+      template: `${SRC_FOLDER}/index.html`,
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
 };
