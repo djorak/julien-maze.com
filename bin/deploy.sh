a4aba7fe9c2aebcfc0a4f64f3263b5603521b582
Julien Mazé <julien.maze@bbc.co.uk>
2021-10-05 11:07:23 +0100

    move to yarn, update travis conf, update build, update deps

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,14 +2,13 @@
 
 echo "-- Deploying www.julien-maze.com to Github pages --"
 
-if [ "$TRAVIS" = "true" ]
-then
+if [ "$TRAVIS" = "true" ]; then
     echo "-> Setting up git user"
     git config --global user.email "travis-ci@localhost"
     git config --global user.name "djorak"
 fi
 
 echo "-> Pushing /dist to Github pages"
-./node_modules/.bin/gh-pages \
+yarn gh-pages \
     --dist dist \
     --repo https://$GH_TOKEN@github.com/djorak/julien-maze.com.git
