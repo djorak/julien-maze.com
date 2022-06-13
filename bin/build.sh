d482c269a61132f20b7b928713cb78fd5771844c
Julien Mazé <julien.maze@wearephlo.com>
2022-06-13 14:07:53 +0100

    chore: update build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,7 @@ echo "-- Building www.julien-maze.com --"
 
 yarn install
 rm -rf dist
-mkdir -p dist
+cp -R src dist
 cp -R node_modules/font-awesome/fonts dist
-cp -R src/index.html dist
-cp -R src/CNAME dist
-cp -R src/files dist
+rm -rf dist/scss
 yarn build:css
