8cb58d57be2b40db390e419a3c86d372e5b5247c
Julien Mazé <mazejul@gmail.com>
2024-12-05 16:44:11 +0100

    Replace twitter link with Bluesky link + other improvements (#10)
    * feat: refactor SCSS imports, update build process and update content
    
    * feat: update CV
    
    * chore: remove irrelevant comment

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ echo "-- Building www.julien-maze.com --"
 yarn install
 rm -rf dist
 cp -R src dist
-cp -R node_modules/font-awesome/fonts dist
 cp -R _layouts dist
 cp _config.yml dist
 rm -rf dist/scss
